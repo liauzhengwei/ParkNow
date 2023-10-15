b66c1d755a80ece708845be220e2b7082ff6729d
Daryl Loh <lohxianjun@gmail.com>
2023-10-15 22:08:23 +0800

    Add react native maps integration

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 1.3483,
+          longitude: 103.6831,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
