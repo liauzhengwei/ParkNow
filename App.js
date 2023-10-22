0c627e6720b7539e9c68772acb095eb9318f27e5
Daryl Loh <lohxianjun@gmail.com>
2023-10-22 22:37:21 +0800

    Add location services

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { useState } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { MaterialIcons } from "@expo/vector-icons";
+import * as Location from "expo-location";
 
 export default function App() {
+  const _map = createRef();
+  const [mapCamera, setMapCamera] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.Balanced,
+          timeInterval: 10000,
+          distanceInterval: 1,
+        },
+        (location) => {
+          setMapCamera({
+            center: {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            },
+            zoom: 17,
+          });
+        }
+      );
+    })();
+  }, []);
+
+  useEffect(() => {
+    _map.current.setCamera(mapCamera);
+  }, [mapCamera]);
+
+  function goToCurrentLoc() {
+    return (
+      <View style={styles.myLocationView}>
+        <TouchableOpacity onPress={getCurrentLocation}>
+          <MaterialIcons name="my-location" size={40} color="gray" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  const getCurrentLocation = () => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      const location = await Location.getLastKnownPositionAsync({});
+      setMapCamera({
+        center: {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        },
+        zoom: 17,
+      });
+    })();
+  };
+
   return (
     <View style={styles.container}>
+      {goToCurrentLoc()}
       <MapView
+        ref={_map}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 1.3397,
-          longitude: 103.7067,
-          latitudeDelta: 0.00922,
-          longitudeDelta: 0.00421,
-        }}
+        showsUserLocation={true}
+        showsMyLocationButton={false}
       />
       <StatusBar style="auto" />
     </View>
@@ -29,7 +90,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   map: {
+    zIndex: -1,
     width: "100%",
     height: "100%",
+    paddingTop: 100,
+  },
+  myLocationView: {
+    alignItems: "flex-end",
+    position: "absolute",
+    bottom: 8.0,
+    right: 8.0,
+    backgroundColor: "rgb(255, 255, 255)",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "gray",
   },
 });
